dc0c23f4c9f3d8bd4826006b99cedaa7341975bf
LuizMilare <lhmm2005@gmail.com>
2026-03-23 17:32:26 -0300

    feat: adicionando paginação na etapa de ingestão (camada bronze)

--- scripts/ingest_breweries.py
+++ scripts/ingest_breweries.py
@@ -5,27 +5,34 @@ from typing import List, Dict
 
 def fetch_breweries():
     base_url = "https://api.openbrewerydb.org/v1/breweries"
+    all_data = []
+    page = 1
+    items_per_page = 200
+
+    while True:
+        print(f"Buscando página {page}...")
+        response = requests.get(base_url, params={"page": page, "per_page": items_per_page})
     
-    print("Buscando dados da API...")
-    response = requests.get(base_url)
-
-    if response.status_code == 200:
+        if response.status_code != 200:
+            print(f"Erro na página {page}: {response.status_code}.")
+            break
+        
         data = response.json()
-        print(f"Sucesso! {len(data)} cervejarias encontradas.")
-        return data
-    else:
-        print(f"Erro na API: {response.status_code}")
-        return None
 
-def save_to_bronze(data):
-    os.makedirs('data/bronze', exist_ok=True)   
+        if not data:
+            break
+        
+        all_data.extend(data)
+        page += 1
 
-    file_path = 'data/bronze/breweries_raw.json'
-    with open(file_path, 'w', encoding='utf-8') as f:
-        json.dump(data, f, ensure_ascii=False, indent=4)
-    print(f"Dados salvos em {file_path}")
+        if page > 1000:
+            break
+
+    os.makedirs("data/bronze", exist_ok=True)
+    with open("data/bronze/breweries_raw.json", "w") as f:
+        json.dump(all_data, f, indent=4)
+
+    print(f"Ingestão concluída com {len(all_data)} registros.")
 
 if __name__ == "__main__":
-    breweries_data = fetch_breweries()
-    if breweries_data:
-        save_to_bronze(breweries_data)
+    fetch_breweries()
